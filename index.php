898882afa69c6b458640ee1808d4f69299ace7bc
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-09-23 21:58:02 +0300

    Debut du section apropos

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-    include "pages/accueil.php"
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blu Azur</title>
+
+    <!-- Import Mulish depuis Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Mulish', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <?php include "pages/accueil.php"; ?>
+</body>
+</html>
--- pages/accueil.php
+++ pages/accueil.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/accueil/header.css">
+    <link rel="stylesheet" href="assets/css/accueil/apropos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Blu Azur</title>
     <style>
@@ -18,6 +19,7 @@
     </style>
 </head>
 <body>
+    <!-- ENTETE DE LA PAGE -->
     <div class="header">
         <div class="menu">
             <div class="d-flex justify-content-between align-items-center px-4 py-3 header-menu">
@@ -74,9 +76,40 @@
             </div>
             <button class="carousel-next btn-circle">></button>
         </div>
-
-
     </div>
-    <script src="assets/js/carousel.js"></script>
+
+    <!-- CORP DE LA PAGE -->
+
+    <div class="apropos d-flex align-items-center pe-5" id="apropos" style="gap: 2rem;">
+        <div class="propos col-7 h bg-red ms-auto d-flex flex-column justify-content-center align-items-start text-black p-5" style="height: 300px;">
+            <h1>Le confort et la sérénité d’un </h1>
+            <h1 class="fw-bold pb-3">appartement privé</h1>
+            <p class="pe-extra">
+                Blu Azur vous propose des appartements élégants et spacieux, conçus pour accueillir de 2 à 6 personnes. Offrant un cadre confortable et moderne, ils sont parfaits pour une escapade détente ou un séjour prolongé.
+            </p>
+            <div class="pt-4">
+                <h4>Les inclusions:</h4>
+                <ul class="pe-extra custom-list">
+                    <li>Stationnement privé</li>
+                    <li>Piscine extérieure avec chaises longues</li>
+                    <li>Linge de lit et serviettes</li>
+                    <li>Télévision, téléphone et Wi-Fi (accès au salon)</li>
+                    <li>Réfrigérateur, cuisinière, lave-vaisselle, vaisselle</li>
+                    <li>Kit bébé: comprend un lit parapluie avec un vrai matelas et une chaise haute</li>
+                    <li>Autres commodités en prêt à la réception: jeux de société, livres, raquettes et balles de ping-pong, sèche-cheveux, fer et planche à repasser, adaptateur pour les prises étrangères et cache-prises pour les enfants.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-4 h bg-red me-0">
+            <!-- Contenu de la colonne droite -->
+        </div>
+    </div>
+
+
+
+
+    <script src="assets/js/carousel_header.js"></script>
+    <script src="assets/js/carousel_apropos.js"></script>
 </body>
 </html>
